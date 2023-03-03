df560bb08602834f1d9850c74a0808d5dddd0e98
Nasif Shahriar Mohim <53572742+shahriarnasif@users.noreply.github.com>
2023-03-03 20:08:23 +0600

    2nd commit
    Company: This model represents a company that can use the app to track its devices. It has fields for the company name, address, and phone number.
    Employee: This model represents an employee who is associated with a company. It has a one-to-one relationship with the built-in User model, which stores information about the employee's login credentials. It also has a foreign key to the Company model.
    Device: This model represents a corporate asset such as a phone, tablet, laptop, or other gear. It has fields for the device model, serial number, device type, purchase date, and condition.
    DeviceLog: This model represents a log entry for a device that has been checked out and returned by an employee. It has a foreign key to the Device model and the Employee model, as well as fields for the check-out and check-in dates, and the condition of the device at check-out and check-in.

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Device(models.Model):
+    DEVICE_TYPE_CHOICES = [
+        ('Phone', 'Phone'),
+        ('Tablet', 'Tablet'),
+        ('Laptop', 'Laptop'),
+        ('Other', 'Other'),
+    ]
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100)
+    device_type = models.CharField(max_length=10, choices=DEVICE_TYPE_CHOICES)
+    purchase_date = models.DateField()
+    condition = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.model
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    check_out_date = models.DateTimeField(auto_now_add=True)
+    check_in_date = models.DateTimeField(null=True, blank=True)
+    check_out_condition = models.CharField(max_length=100, null=True, blank=True)
+    check_in_condition = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.device.model} - {self.employee.user.username}'
+
+
+
